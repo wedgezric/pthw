fb5583dd4067f31e25e7da1f593f8487abad17a0
wedgezric <wedgezric@users.noreply.github.com>
2015-05-23 17:45:33 -0400

    ex1.py
    # ex1.py for pthw

---
+++ ex1.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+#print "Hello World!"
+print "Hello Again"
+print "I like typing this."
+print "This is fun."
+print 'Yay! Printing.'
+print "I'd much rather you 'not'."
+print 'I "said" do not touch this.'
+print 'This is the end of this lesson.'
